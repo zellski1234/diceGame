2b02d9f693f698996bace6f9571a06d74388cb4d
zellski1234 <zellski1234@gmail.com>
2022-07-28 15:29:02 +0100

    updated

--- script.js
+++ script.js
@@ -8,43 +8,29 @@ let scoreNum = document.getElementById(`scoreNum`);
 let score = 0;
 
 
-
-
+let rollsound = new Audio("sound/roll.wav"); 
 
 
 startBtn.addEventListener(`click`, () =>{
-    
-    // if roll button hidden
-    if(rollDiceBtn.style.display == `none`){
         startBtn.textContent = `Restart`
         rollDiceBtn.style.display = `block`
         score = 0
-        scoreNum.textContent = `Your Score is ${score}`;
-
-    }
-    // if roll button visble
-    else {
-        rollDiceBtn.style.display = `block`
-        startBtn.textContent = `Restart`
-        score = 0
-        scoreNum.textContent = `Your Score is ${score}`;
-        diceNum.textContent = ``        
-    }
+        scoreNum.textContent = `Score: ${score}`;
+        diceNum.textContent = `` 
 });
-
-
 
 
 rollDiceBtn.addEventListener(`click`, () =>{
 
     let diceRoll = Math.ceil(Math.random() * 6);
+    rollsound.play();
 
     if(diceRoll > 1 && score < 20){
         diceNum.textContent = `You rolled a ${diceRoll}`
         score = score + diceRoll;
         console.log(score);
         console.log(`rolled a ${diceRoll}`);
-        scoreNum.textContent = `Your Score is ${score}`;
+        scoreNum.textContent = `Score: ${score}`;
         if (score >= 20){
             scoreNum.textContent = `You won with a score of ${score}`;
             rollDiceBtn.style.display = `none`
@@ -54,7 +40,7 @@ rollDiceBtn.addEventListener(`click`, () =>{
     else if (diceRoll == 1) {
         
         score = 0;
-        scoreNum.textContent = `You lost score: ${score}`;
+        scoreNum.textContent = `You lost Score: ${score}`;
         diceNum.textContent = `You rolled a ${diceRoll}`
         console.log(`rolled a ${diceRoll}`);
         console.log("lost");
